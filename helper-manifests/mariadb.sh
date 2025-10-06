9ab499889dc9068c47feb0bc94a053f3799337d4
mrmysko <orkar_-.-@hotmail.com>
2025-10-06 17:18:56 +0000

    cleanup

--- helper-manifests/mariadb.sh
+++ helper-manifests/mariadb.sh
@@ -1,1 +1,4 @@
+#!/bin/bash
+
+# Drop into a mariadb pod
 kubectl run mariadb-shell --rm -it --image=mariadb:11.8.2 -- /bin/bash
--- helper-manifests/netshoot.sh
+++ helper-manifests/netshoot.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-# Drop into a netshoot pod
-
+# Drop into a netshoot pod for network troubleshooting
 kubectl run netshoot --image=nicolaka/netshoot --restart=Never --rm -it -- bash
